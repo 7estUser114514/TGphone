efb9850c4e9f84019619cf70a237ed23f7551706
7estUser114514 <testUser2024aaa@proton.me>
2024-08-23 10:46:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import webbrowser
 from flask import Flask, render_template
 app = Flask(__name__)
  
@@ -6,4 +7,5 @@ def index():
    return app.send_static_file("index.html")
  
 if __name__ == '__main__':
-   app.run(debug = True)
+   webbrowser.open("http://127.0.0.1:5678")
+   app.run(port=5678)
